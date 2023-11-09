d071c1db56e0d80b9733ac7018899e25b0e4c2df
toavakam <toavakam@gmail.com>
2023-11-09 11:18:24 +0200

    components

--- resources/views/questions.blade.php
+++ resources/views/questions.blade.php
@@ -61,62 +61,12 @@
             </ul>
         </div>
     @endif
-    <h2>{{ $question['text'] }}</h2>
-    @if(isset($question['image']))
-        <img src="{{ $question['image'] }}">
-    @endif
-    <p>{{ $question['description'] }}</p>
-    @if($question['type'] === 'multiple-choice')
-        <form method="post" action="{{ route('answer', ['pk' => $pk, 'num' => $num, 'lang'=>$lang]) }}">
-            @csrf
-            @foreach($question['answers'] as $answer)
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="answer_{{ $answer['id'] }}" name="a{{ $question['id'] }}[]" value="{{ $answer['id'] }}"
-                           @if(isset($userAnswer) && in_array($answer['id'], $userAnswer)) checked @endif>
-                    <label class="form-check-label" for="answer_{{ $answer['id'] }}">{{ $answer['value'] }}</label>
-                </div>
-            @endforeach
-            @if($num > 1)
-                <a href="{{ route('question', ['pk' => $pk, 'num' => $num - 1, 'lang' => $lang]) }}" class="btn btn-primary mt-4">{{ __('messages.previous_question') }}</a>
-            @endif
-            <button type="submit" class="btn btn-primary mt-4">{{ __('messages.next_question') }}</button>
-        </form>
-    @elseif($question['type'] === 'single-choice')
-        <form method="post" action="{{ route('answer', ['pk' => $pk, 'num' => $num, 'lang'=>$lang]) }}">
-            @csrf
-            @foreach($question['answers'] as $answer)
-                <div class="form-check">
-                    <input type="radio" class="form-check-input" id="answer_{{ $answer['id'] }}" name="answer" value="{{ $answer['id'] }}"
-                           @if(isset($userAnswer) && $userAnswer == $answer['id']) checked @endif>
-                    <label class="form-check-label" for="answer_{{ $answer['id'] }}">{{ $answer['value'] }}</label>
-                </div>
-            @endforeach
-            @if($num > 1)
-                <a href="{{ route('question', ['pk' => $pk, 'num' => $num - 1, 'lang' => $lang]) }}" class="btn btn-primary mt-4">{{ __('messages.previous_question') }}</a>
-            @endif
-            <button type="submit" class="btn btn-primary mt-4">{{ __('messages.next_question') }}</button>
-        </form>
+    @if($question['type'] === 'single-choice')
+        @include('components.single_choice_question', ['question' => $question, 'pk' => $pk, 'num' => $num, 'lang' => $lang, 'userAnswer' => $userAnswer])
+    @elseif($question['type'] === 'multiple-choice')
+        @include('components.multiple_choice_question', ['question' => $question, 'pk' => $pk, 'num' => $num, 'lang' => $lang, 'userAnswer' => $userAnswer])
     @elseif($question['type'] === 'order')
-        <form method="post" action="{{ route('answer', ['pk' => $pk, 'num' => $num, 'lang'=>$lang]) }}">
-            @csrf
-            @foreach($question['answers'] as $answer)
-                <div class="mb-3">
-                    <label for="answer_{{ $answer['id'] }}" class="form-label">{{ $answer['value'] }}</label>
-                    <div class="dropdown">
-                        <select class="form-select" aria-label="Default select example" name="{{ $question['id'] }}[{{ $answer['id'] }}]">
-                            <option value="">---</option>
-                            @for($i = 1; $i <= count($question['answers']); $i++)
-                                <option value="{{ $i }}" @if(isset($userAnswer[$answer['id']]) && $userAnswer[$answer['id']] == $i) selected @endif>{{ $i }}</option>
-                            @endfor
-                        </select>
-                    </div>
-                </div>
-            @endforeach
-            @if($num > 1)
-                <a href="{{ route('question', ['pk' => $pk, 'num' => $num - 1, 'lang' => $lang]) }}" class="btn btn-primary mt-4">{{ __('messages.previous_question') }}</a>
-            @endif
-            <button type="submit" class="btn btn-primary mt-4">{{ __('messages.next_question') }}</button>
-        </form>
+        @include('components.order_question', ['question' => $question, 'pk' => $pk, 'num' => $num, 'lang' => $lang, 'userAnswer' => $userAnswer])
     @endif
 </div>
 </body>
