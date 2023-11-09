9b2260f0db9015d07bc67bb51ffa620589b04725
Pavel Musolin <pavel@knigun.com>
2023-11-09 11:19:20 +0200

    🐛 FIX: migration

--- database/migrations/2023_11_09_082646_add_question_id_nullable.php
+++ database/migrations/2023_11_09_082646_add_question_id_nullable.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('results', function (Blueprint $table) {
-            $table->unsignedSmallInteger('question_id')->nullable()->change();
+            $table->unsignedSmallInteger('question_id')->nullable();
 
         });
     }
